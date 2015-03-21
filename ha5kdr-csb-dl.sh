7948c664516cc7a280ffca4bbb9226f9906ecac8
Nonoo <nonoo@nonoo.hu>
2015-03-21 09:13:35 +0000

    sending notification email when csb dl fails

--- ha5kdr-csb-dl.sh
+++ ha5kdr-csb-dl.sh
@@ -10,5 +10,7 @@ scriptdir=${self%$scriptname}
 wget 'http://nmhh.hu/amator/call_sign_book.xml' -O /tmp/csb.xml &>/dev/null
 if [ $? -eq 0 ]; then
 	$scriptdir/ha5kdr-csb-process.php /tmp/csb.xml
+else
+	echo "Error while downloading csb!" | mail -s "[`hostname`] error downloading csb" nonoo@nonoo.hu
 fi
 rm -f /tmp/csb.xml
