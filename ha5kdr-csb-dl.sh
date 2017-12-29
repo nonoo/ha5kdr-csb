b2070192a7ed97d65be7a8d47cacd7b3916b12be
István Nyitrai <i.nyitrai@mito.hu>
2017-12-29 11:56:04 +0100

    Change file encoding after download

--- ha5kdr-csb-dl.sh
+++ ha5kdr-csb-dl.sh
@@ -8,5 +8,8 @@ scriptname=`basename "$self"`
 scriptdir=${self%$scriptname}
 
 wget 'http://nmhh.hu/amator/call_sign_book.xml' -O /tmp/csb.xml &>/dev/null
+iconv -f ISO-8859-2 -t UTF-8 /tmp/csb.xml > /tmp/csb_tmp.xml
+mv /tmp/csb_tmp.xml /tmp/csb.xml
+sed -i'' -e 's/ISO-8859-1/UTF-8/g' /tmp/csb.xml
 $scriptdir/ha5kdr-csb-process.php /tmp/csb.xml
 rm -f /tmp/csb.xml
